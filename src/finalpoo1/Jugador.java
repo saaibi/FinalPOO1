6759efb28e31155b94a3c454ac176d8a36c061d9
saaibi yesid florez <saaibiyesid@hotmail.com>
2017-11-05 11:03:01 -0500

    first commit

---
+++ src/finalpoo1/Jugador.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalpoo1;
+
+/**
+ *
+ * @author DAVID
+ */
+public class Jugador {
+    
+    String nombre;
+    String color;
+
+    public Jugador() {
+    }
+
+    public Jugador(String nombre, String Color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
+    
+    public void tirarDado(int dado) {
+        
+    }
+    
+}
